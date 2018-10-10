5c0a2d9ff696322e058e91dff6c3565c81a924c0
josephiznot <josephiznot@gmail.com>
2018-10-09 22:29:52 -0500

    disabled autoformat

--- src/components/signup-form.js
+++ src/components/signup-form.js
@@ -19,7 +19,7 @@ const rootStyles = {
   flexDirection: 'column',
   alignItems: 'center',
   maxWidth: 350,
-};
+}
 
 // Local Functions
 const byPropKey = (propertyName, value) => () => ({
@@ -34,12 +34,16 @@ class SignUpForm extends Component {
     this.state = { ...INITIAL_STATE };
   }
 
-  onSubmit = event => {
-    const { username, email, passwordOne } = this.state;
+  onSubmit = (event) => {
+    const {
+      username,
+      email,
+      passwordOne,
+    } = this.state;
 
-    auth
-      .doCreateUserWithEmailAndPassword(email, passwordOne)
+    auth.doCreateUserWithEmailAndPassword(email, passwordOne)
       .then(authUser => {
+
         // Create a user in your own accessible Firebase Database too
         db.doCreateUser(authUser.user.uid, username, email)
           .then(() => {
@@ -57,48 +61,49 @@ class SignUpForm extends Component {
       });
 
     event.preventDefault();
-  };
+  }
 
   render() {
-    const { username, email, passwordOne, passwordTwo, error } = this.state;
+    const {
+      username,
+      email,
+      passwordOne,
+      passwordTwo,
+      error,
+    } = this.state;
 
     const isInvalid =
       passwordOne !== passwordTwo ||
       passwordOne === '' ||
-      !/^\w+@\w+\.\w+$/.test(email) ||
+      !/^\w+@\w+\.\w+$/.test(email) === '' ||
       username === '';
 
     return (
-      <form css={rootStyles} onSubmit={this.onSubmit}>
+      <form
+        css={rootStyles}
+        onSubmit={this.onSubmit}
+      >
         <input
           value={username}
-          onChange={event =>
-            this.setState(byPropKey('username', event.target.value))
-          }
+          onChange={event => this.setState(byPropKey('username', event.target.value))}
           type="text"
           placeholder="Full Name"
         />
         <input
           value={email}
-          onChange={event =>
-            this.setState(byPropKey('email', event.target.value))
-          }
+          onChange={event => this.setState(byPropKey('email', event.target.value))}
           type="text"
           placeholder="Email Address"
         />
         <input
           value={passwordOne}
-          onChange={event =>
-            this.setState(byPropKey('passwordOne', event.target.value))
-          }
+          onChange={event => this.setState(byPropKey('passwordOne', event.target.value))}
           type="password"
           placeholder="Password"
         />
         <input
           value={passwordTwo}
-          onChange={event =>
-            this.setState(byPropKey('passwordTwo', event.target.value))
-          }
+          onChange={event => this.setState(byPropKey('passwordTwo', event.target.value))}
           type="password"
           placeholder="Confirm Password"
         />
@@ -106,7 +111,7 @@ class SignUpForm extends Component {
           Sign Up
         </button>
 
-        {error && <p>{error.message}</p>}
+        { error && <p>{error.message}</p> }
       </form>
     );
   }
