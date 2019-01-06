a1108764e9c2f91b9c44efd81ebe4c057ceeebf3
Mike Mathew <m2mathew@me.com>
2019-01-05 23:13:26 -0600

    [Pages] Puts old event into a prototype card

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,4 +1,7 @@
 // External Dependencies
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
@@ -23,7 +26,7 @@ const styles = theme => ({
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'center',
-    padding: '3.5rem 0',
+    padding: '3.5rem',
   },
   heroContainer: {
     background: '#282C34',
@@ -35,6 +38,16 @@ const styles = theme => ({
   },
   heroTitle: {
     color: '#61dafb',
+  },
+  media: {
+    height: '50%',
+    width: '50%',
+  },
+  mediaContainer: {
+    background: '#f1f1f1',
+    display: 'flex',
+    justifyContent: 'center',
+    paddingTop: 24,
   },
   root: {
     textAlign: 'center',
@@ -54,16 +67,29 @@ const Events = ({ classes }) => {
         </section>
 
         <section className={classes.content}>
-          <h3>December 2018</h3>
-          <h4>ReactJS @ BottleRocket</h4>
-          <div>6:15 - 7:00pm: Pizza/Drinks & meeting other people</div>
-          <div>6:30 - 7:00pm: Jobs Open Mic and jobs follow-up</div>
-          <div>7:00 - 7:45pm - Morgan Dedmon - "WASM: What is that?"</div>
-          <div>8:00 - 8:45pm: Salvador Aceves - "Redux Sagas in Practice"</div>
+          <Card>
+            <div className={classes.mediaContainer}>
+              <CardMedia
+                alt="Bottle Rocket Logo"
+                className={classes.media}
+                component="img"
+                image="https://www.bottlerocketstudios.com/assets/br-logo.png"
+                title="Bottle Rocket Logo"
+              />
+            </div>
+            <CardContent>
+              <h3>December 2018</h3>
+              <h4>ReactJS @ BottleRocket</h4>
+              <div>6:15 - 7:00pm: Pizza/Drinks & meeting other people</div>
+              <div>6:30 - 7:00pm: Jobs Open Mic and jobs follow-up</div>
+              <div>7:00 - 7:45pm - Morgan Dedmon - "WASM: What is that?"</div>
+              <div>8:00 - 8:45pm: Salvador Aceves - "Redux Sagas in Practice"</div>
 
-          <p style={{ marginTop: 12 }}>
-            <a href="https://www.meetup.com/ReactJSDallas/events/pbbdwnyxqbpb/">Meetup link</a>
-          </p>
+              <p style={{ marginTop: 12 }}>
+                <a href="https://www.meetup.com/ReactJSDallas/events/pbbdwnyxqbpb/">View on meetup.com</a>
+              </p>
+            </CardContent>
+          </Card>
         </section>
 
         <ReactDallasLogoSection />
