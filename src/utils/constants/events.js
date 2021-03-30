2a0ca5996daeec2e362e4891fe941c6fa674637e
Jonathan Martinez <Jmartinez070589@gmail.com>
2021-03-30 05:06:42 -0500

    this commit updates the upcoming event for the april meet

--- src/utils/constants/events.js
+++ src/utils/constants/events.js
@@ -338,7 +338,7 @@ export const futureEventData = [
     imageLink:
       'https://images.unsplash.com/photo-1588702547923-7093a6c3ba33?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
     meetupLink: 'https://www.meetup.com/ReactJSDallas/events/mrkxmryccfbmb/',
-    speaker1: 'Tim Waite - React vs. Vue - the ultimate showdown',
+    speaker1: "Tim Waite - How to choose the right JS framework - And why it's probably React",
     venue: 'yourDevice',
   },
 ];
