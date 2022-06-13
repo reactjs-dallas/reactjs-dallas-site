5fb2a251dfd54b3344f20e589593d947a1cbe3d9
Mike Mathew <m2mathew@me.com>
2022-06-13 16:39:57 -0500

    refactor: add current-event page

---
+++ src/pages/current-event.js
@@ -0,0 +1,16 @@
+// External Dependencies
+import React from 'react';
+
+// Internal Dependencies
+import Layout from '../components/layout';
+
+// Component Definition
+const CurrentEvent = () => (
+  <Layout hideFooter hideNav>
+    <div>
+      <img width="100%" height="100%" src="https://res.cloudinary.com/reactjs-dallas/image/upload/v1655155227/2022-06-13_-_react_meetup_thumbnail_en9dlo.png"/>
+    </div>
+  </Layout>
+);
+
+export default CurrentEvent;
