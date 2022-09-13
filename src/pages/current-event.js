888bf13df7eef3925dd59b3d47eaaf9028cdf583
Mike Mathew <m2mathew@me.com>
2022-09-13 17:40:18 -0500

    chore: update September 2022 event

--- src/pages/current-event.js
+++ src/pages/current-event.js
@@ -8,7 +8,7 @@ import Layout from '../components/layout';
 const CurrentEvent = () => (
   <Layout hideFooter hideNav>
     <div>
-      <img alt="meetup-speaker" width="100%" height="100%" src="https://res.cloudinary.com/reactjs-dallas/image/upload/v1660078702/2022-08-09_-_ReactJS_Dallas_featuring_Mike_Mathew_llj2iu.png"/>
+      <img alt="meetup-speaker" width="100%" height="100%" src="https://res.cloudinary.com/reactjs-dallas/image/upload/v1663108736/2022-09-13--react_meetup_xjirma.png"/>
     </div>
   </Layout>
 );
