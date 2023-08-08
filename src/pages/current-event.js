5a30ad3c280d9acf17a4abd9ddf3438980701cea
Mike Mathew <m2mathew@me.com>
2023-08-08 12:58:09 -0500

    chore(events): add event data for August 2023 event at Text-Em-All

--- src/pages/current-event.js
+++ src/pages/current-event.js
@@ -11,7 +11,7 @@ const CurrentEvent = () => (
       <img
         alt="meetup-speaker"
         height="100%"
-        src="https://res.cloudinary.com/reactjs-dallas/image/upload/v1689104985/2023-07-11--ReactJS-Dallas-with-Joe-Warren_wgtspo.png"
+        src="https://res.cloudinary.com/reactjs-dallas/image/upload/v1691517314/2023-08-08--reactjs_dallas_with_james_vogel_ygztis.png"
         width="100%"
       />
     </div>
