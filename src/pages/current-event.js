5403e8a857e1efcb4a221b1e9bbb8d0345acfe7d
Mike Mathew <m2mathew@me.com>
2023-09-12 17:09:00 -0500

    chore(events): update with September 2023 event details

--- src/pages/current-event.js
+++ src/pages/current-event.js
@@ -11,7 +11,7 @@ const CurrentEvent = () => (
       <img
         alt="meetup-speaker"
         height="100%"
-        src="https://res.cloudinary.com/reactjs-dallas/image/upload/v1691517314/2023-08-08--reactjs_dallas_with_james_vogel_ygztis.png"
+        src="https://res.cloudinary.com/reactjs-dallas/image/upload/v1694556369/2023-09-12--reactjs_dallas_with_patrick_hulce_ohfmm2.png"
         width="100%"
       />
     </div>
