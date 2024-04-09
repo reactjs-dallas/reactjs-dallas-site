bf1494e7a9fd5e10e2d01dcc39c6962112673c12
Mike Mathew <m2mathew@me.com>
2024-04-09 14:16:36 -0500

    chore: update April 2024 event details

--- src/pages/current-event.js
+++ src/pages/current-event.js
@@ -11,7 +11,7 @@ const CurrentEvent = () => (
       <img
         alt="meetup-speaker"
         height="100%"
-        src="https://res.cloudinary.com/reactjs-dallas/image/upload/v1710254183/2024-03-12--reactjs_with_mark_mulligan_qxgybv.png"
+        src="https://res.cloudinary.com/reactjs-dallas/image/upload/v1712689786/2024-04-09--reactjs_dallas_with_mike_mathew_group_project_thkkla.png"
         width="100%"
       />
     </div>
